5d733d7f158e1af35e6a1e13735f3cd6da9e95b1
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-06-11 01:21:16 +0300

    p5

---
+++ P1/S1-p5.c
@@ -0,0 +1,12 @@
+/*
+
+Problema e prea simpla sa mi bat capul. Faci un arbore binar 
+cu noduri care pastreaza informatia care ti se cere.
+Arborele este ordonat dupa id-ul cursului. La salvare
+in lista(tablou) trebuie parcurs in inordine (Left ROOT Right)
+ca sa le poti baga direct in ordine. That's it. Doar multe date de scris.
+
+Pentru adaugare in lista e O(n), n numarul total de noduri.
+Pentru adaugare in arbore e O(log n)
+
+*/
